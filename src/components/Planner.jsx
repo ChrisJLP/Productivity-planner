5b4487648110c90c2b85a444357b0658d6a5d675
Chris Pring <chrisjlpring@outlook.com>
2024-06-25 16:10:50 +0100

    Add basic structure to Planner component

--- src/components/Planner.jsx
+++ src/components/Planner.jsx
@@ -1,15 +1,47 @@
 import { useState } from "react";
 
 function Planner() {
-  const { taskInputVal, setTaskInputVal } = useState("");
-  const { workTimeVal, setWorkTimeVal } = useState("");
+  const [taskInputVal, setTaskInputVal] = useState("");
+  const [workTimeVal, setWorkTimeVal] = useState(60);
+  const [defaultTimeVal, setDefaultTimeVal] = useState(25);
+
+  const handleTaskChange = (e) => {
+    setTaskInputVal(e.target.value);
+  };
+
+  const handleTimeChange = (e) => {
+    setWorkTimeVal(e.target.value);
+  };
+
+  const handleDefaultTimeChange = (e) => {
+    setDefaultTimeVal(e.target.value);
+  };
 
   return (
     <>
       <p>What do you want to achieve today?</p>
-      <input type="text" name="task-entry" value={taskInputVal} />
+      <input
+        type="text"
+        name="task-entry"
+        value={taskInputVal}
+        onChange={handleTaskChange}
+      />
       <p>How long do you want to work today?</p>
-      <input type="text" name="work-time-entry" value={workTimeVal} />
+      <input
+        type="number"
+        name="work-time-entry"
+        value={workTimeVal}
+        min="10"
+        onChange={handleTimeChange}
+      />
+      <p>Default timer length?</p>
+      <input
+        type="number"
+        name="default-time-entry"
+        value={defaultTimeVal}
+        min="10"
+        onChange={handleDefaultTimeChange}
+      />
     </>
   );
 }
